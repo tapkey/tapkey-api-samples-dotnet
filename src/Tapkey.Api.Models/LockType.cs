f44344d764835ac2ebb68db930a35c48a7c7c269
Joao Grassi <joaop.grassi@gmail.com>
2019-02-22 15:53:44 +0100

    Added a project to keep the models classes compatible with the Tapkey Management API.

---
+++ src/Tapkey.Api.Models/LockType.cs
@@ -0,0 +1,11 @@
+﻿namespace Tapkey.Api.Models
+{
+    public class LockType
+    {
+        public string Id { get; set; }
+
+        public int ManufacturerId { get; set; }
+
+        public string ModelName { get; set; }
+    }
+}
